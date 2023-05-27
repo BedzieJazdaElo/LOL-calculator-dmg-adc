3767f9e01e1869b81fcada8d91283300b475e9d4
BedzieJazdaElo <125198635+BedzieJazdaElo@users.noreply.github.com>
2023-05-27 22:35:45 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-
+#include "calculator_dmg.hpp"
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    character_lol aatrox{580, 38, 32};
+    std::cout<<aatrox.hp-dmg_calculator({100, 100, 100}, aatrox)<<std::endl; // output: 331.779
 }
