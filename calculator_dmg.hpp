17c43dd4535cfc89ea23bbd439e95269c4aabc1e
BedzieJazdaElo <125198635+BedzieJazdaElo@users.noreply.github.com>
2023-05-30 22:45:25 +0200

    new file:   .vscode/settings.json 	modified:   calculator_dmg.hpp 	new file:   dane.csv 	modified:   main.cpp

--- calculator_dmg.hpp
+++ calculator_dmg.hpp
@@ -1,37 +1,107 @@
-struct character_lol
-{//stats of lol character
-    float hp{}, armor{}, magic_resist{};
-    
-};
+#pragma once
+#include<iostream>
+#include <fstream>
+#include <string>
 
-
-struct adc_lol
-{//stats of lol adc
-    float ad{}, as{}, crit{}, lifesteal{}, flat_armor_penetration{}, percent_armor_penetration{}, flat_magic_resist_penetration{}, percent_magic_resist_penetration{}, hp{};
-};
-
-
-struct dmg
-{
-    float physical_dmg{}, magic_dmg{}, true_dmg{}, percent_physical_dmg{}, percent_magic_dmg{}, percent_true_dmg{}, flat_armor_penetration{}, percent_armor_penetration{}, flat_magic_resist_penetration{}, percent_magic_resist_penetration{};
-};
+using units=float;
 
 // equation for calculating damage after armor and magic resist
-float after_resist(float dmg, float resist) // https://leagueoflegends.fandom.com/wiki/Armor , the same for magic resist
+units DMG_after_resist(units dmg, units resist) // https://leagueoflegends.fandom.com/wiki/Armor , the same for magic resist
 {
-    if(resist >= 0)
+    if (resist >= 0)
     {
-        return dmg * (100/(100+resist));
+        return dmg * (100 / (100 + resist));
     }
     else
     {
-        return 2 - (100/(100-resist));
+        return 2 - (100 / (100 - resist));
     }
 }
 
-
-
-float dmg_calculator(dmg damage, character_lol defender)
+class Champion_class
 {
-    return after_resist(damage.percent_magic_dmg * defender.hp, defender.magic_resist*(1-damage.percent_magic_resist_penetration)-damage.flat_magic_resist_penetration) + after_resist(damage.percent_physical_dmg * defender.hp, defender.armor*(1-damage.percent_armor_penetration)-damage.flat_armor_penetration) + damage.percent_true_dmg * defender.hp+ after_resist(damage.magic_dmg, defender.magic_resist) + after_resist(damage.physical_dmg, defender.armor) + damage.true_dmg;
-}
+    units health, healt_regeneration,// per 5 sec
+     armor, magic_resist, movement_speed, mana, mana_regeneration, attack_damage, crit_damage, attack_range, base_AS, attack_windup, AS_ratio=1;
+
+    units healthPerLevel, healt_regenerationPerLevel, armorPerLevel, magic_resistPerLevel, manaPerLevel, mana_regenerationPerLevel, attack_damagePerLevel, AS_PerLevel;
+
+    public:
+
+    Champion_class(units health, units healt_regeneration, units armor, units magic_resist, units movement_speed, units mana, units mana_regeneration, units attack_damage, units crit_damage, units attack_range, units base_AS, units attack_windup, units AS_ratio, units healthPerLevel, units healt_regenerationPerLevel, units armorPerLevel, units magic_resistPerLevel, units manaPerLevel, units mana_regenerationPerLevel, units attack_damagePerLevel, units AS_PerLevel)
+    {
+        this->health = health;
+        this->healt_regeneration = healt_regeneration;
+        this->armor = armor;
+        this->magic_resist = magic_resist;
+        this->movement_speed = movement_speed;
+        this->mana = mana;
+        this->mana_regeneration = mana_regeneration;
+        this->attack_damage = attack_damage;
+        this->crit_damage = crit_damage;
+        this->attack_range = attack_range;
+        this->base_AS = base_AS;
+        this->attack_windup = attack_windup;
+        this->AS_ratio = AS_ratio;
+        this->healthPerLevel = healthPerLevel;
+        this->healt_regenerationPerLevel = healt_regenerationPerLevel;
+        this->armorPerLevel = armorPerLevel;
+        this->magic_resistPerLevel = magic_resistPerLevel;
+        this->manaPerLevel = manaPerLevel;
+        this->mana_regenerationPerLevel = mana_regenerationPerLevel;
+        this->attack_damagePerLevel = attack_damagePerLevel;
+        this->AS_PerLevel = AS_PerLevel;
+    }
+    
+    Champion_class(std::string name)
+    {
+        std::ifstream file("dane.csv");
+        std::string line;
+        while(std::getline(file, line,';'));
+    }
+
+    units outputMovementSpeed()
+    {
+        return movement_speed;
+    }
+//
+    units outputAttackSpeed(uint8_t level, units bonusAttackSpeed)// https://leagueoflegends.fandom.com/wiki/Champion_statistic#Attack_speed_calculations
+    {
+        return base_AS + (AS_ratio+((AS_PerLevel*(level-1)*(0.7025+0.0175*(level-1)))+bonusAttackSpeed));
+    }
+
+    units outputHealth(uint8_t level)
+    {
+        return health + (healthPerLevel*(level-1));
+    }
+
+    units outputHealthRegeneration(uint8_t level)
+    {
+        return healt_regeneration + (healt_regenerationPerLevel*(level-1));
+    }
+
+    units outputArmor(uint8_t level)
+    {
+        return armor + (armorPerLevel*(level-1));
+    }
+
+    units outputMagicResist(uint8_t level)
+    {
+        return magic_resist + (magic_resistPerLevel*(level-1));
+    }
+
+    units outputMana(uint8_t level)
+    {
+        return mana + (manaPerLevel*(level-1));
+    }
+
+    units outputManaRegeneration(uint8_t level)
+    {
+        return mana_regeneration + (mana_regenerationPerLevel*(level-1));
+    }
+
+    units outputAttackDamage(uint8_t level)
+    {
+        return attack_damage + (attack_damagePerLevel*(level-1));
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
+#include <fstream>
 #include "calculator_dmg.hpp"
-
 int main()
 {
-    character_lol aatrox{580, 38, 32};
-    std::cout<<aatrox.hp-dmg_calculator({100, 100, 100}, aatrox)<<std::endl; // output: 331.779
+
 }
